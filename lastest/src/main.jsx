ec2954194ddb96cd9e323e3916c5dc5ceb840bd6
MARIA EUGENIA LUCERO <48313612@ort>
2025-04-10 12:05:38 -0300

    jsx

--- lastest/src/main.jsx
+++ lastest/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Cita from '../modules/Cita.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Cita />
   </StrictMode>,
 )
